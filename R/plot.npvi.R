63e00b34783d3175514ed377b2bf0059d110c80f
bdwilliamson <brianw26@uw.edu>
2017-04-05 14:01:46 -0700

    Updated plot.npvi() to handle graphical parameters in a more logical way, and handle plot calls from a single npvi object

--- R/plot.npvi.R
+++ R/plot.npvi.R
@@ -2,22 +2,52 @@
 #'
 #' Plot the estimates and standard errors for a set of \code{npvi} objects.
 #'
-#' @param x an \code{npvi} object created from a call to \code{combine}.
-#' @param y a vector of names, given in the same order as the list.
+#' @param x an \code{npvi} object.
+#' @param y a vector of names, given in the same order as the estimates in the \code{npvi} object.
 #' @param ... other options, see the generic \code{plot} function.
 #' @export
 plot.npvi <- function(x, y, ...) {
 
   ## order based on the estimate
-  ord.mat <- x$mat[order(x$mat$est), ]
+  if (!is.null(x$mat)) {
+    ord.mat <- x$mat[order(x$mat$est), ]
+    if (missing(y)) {
+      tmp <- do.call(c, x$j)
+      y <- paste("j = ", tmp, sep = "")
+    }
+  } else {
+    tmp <- data.frame(est = x$est, se = x$se, x$ci)
+    names(tmp) <- c("est", "se", "cil", "ciu")
+    ord.mat <- tmp[order(tmp$est), ]
+    if (missing(y)) {
+      y <- paste("j = ", x$j, sep = "")
+    }
+  }
+
+
+  ## get the graphical parameters passed in
+  L <- list(...)
+
+  ## set up default parameters
+  if (!("mar" %in% names(L))) {
+    mar <- c(5, 12, 4, 2) + 0.1
+  }
+  if (!("xlim" %in% names(L))) {
+    xlim <- c(0, 1)
+  }
+  if (!("ylim" %in% names(L))) {
+    ylim <- c(0, dim(ord.mat)[1])
+  }
 
   ## plot
-  graphics::par(mar = c(5, 12, 4, 2) + 0.1)
-  graphics::plot(ord.mat$est, 1:dim(ord.mat)[1], ...)
+  graphics::par(mar = mar)
+  graphics::plot.new()
+  graphics::plot.window(xlim, ylim)
+  graphics::points(ord.mat$est, 1:dim(ord.mat)[1], ...)
   graphics::abline(v = 0, col = "red", lty = 2)
   graphics::arrows(unlist(ord.mat$cil), 1:dim(ord.mat)[1], unlist(ord.mat$ciu), 1:dim(ord.mat)[1],
          length = 0, angle = 90, lwd = 2)
   graphics::axis(side = 2, at = 1:dim(ord.mat)[1], label = y[order(ord.mat$est)], las = 2)
-  graphics::axis(side = 1, at = seq(0, 1, 0.1))
+  graphics::axis(side = 1, at = seq(xlim[1], xlim[2], xlim[2]/10))
   graphics::box()
 }
