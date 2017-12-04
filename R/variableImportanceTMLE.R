01efa75ded04d2f135e77b64c5fcf649afecf2d1
Brian Williamson <brianw26@uw.edu>
2017-12-04 13:51:34 -0800

    Merged TMLE2 with TMLE in dev branch

--- R/variableImportanceTMLE.R
+++ R/variableImportanceTMLE.R
@@ -3,19 +3,20 @@
 #' Compute nonparametric estimates of the variable importance parameter interpreted as the proportion of variability explained by including a group of covariates in the estimation technique.
 #'
 #' @param full fitted values from an initial regression of the outcome on the full set of covariates.
-#' @param reduced fitted values from an initial regression either (1) of the outcome on the reduced set of covariates, or (2) of the fitted values from the full regression on the reduced set of covariates.
+#' @param reduced fitted values from an initial regression either (1) of the outcome on the reduced set of covariates, or (2) of the fitted values from the full regression on the reduced set of covariates. \code{reduced} may be a matrix or data frame, in which case multivariate updates will be performed.
 #' @param y the outcome variable
 #' @param x the covariates (ordered)
 #' @param s the indices for variable importance
-#' @param lib the library of candidate learners passed to SuperLearner
+#' @param lib the library of candidate learners passed to Super Learner
 #' @param tol the tolerance level for convergence
+#' @param max_iter the maximum number of iterations for each Super Learner fit
 #' @param ... other arguments, passed to SuperLearner
 #'
 #' @return The estimated variable importance for the given group of left-out covariates, along with the SE and a CI.
 #'
 #' @details This differs from estimates returned by variableImportance() in that the procedure used to target the estimate to the parameter of interest is based on a TMLE approach.
 #' @export
-variableImportanceTMLE <- function(full, reduced, y, x, s, lib, tol = .Machine$double.eps, max.iter = 500, ...) {
+variableImportanceTMLE2 <- function(full, reduced, y, x, s, lib, tol = .Machine$double.eps, max_iter = 500, ...) {
     ## helper functions
     logit <- function(x) log(x/(1-x))
     # expit <- function(x) exp(x)/(1+exp(x))
@@ -36,11 +37,16 @@ variableImportanceTMLE <- function(full, reduced, y, x, s, lib, tol = .Machine$d
     off <- logit(full)
 
     ## get initial epsilon (with no intercept)
-    eps.init <- suppressWarnings(glm(ystar ~ covar - 1, offset = off, family = binomial(link = "logit"))$coefficients[1])
+    fluctuation <- suppressWarnings(glm(ystar ~ covar - 1, offset = off, family = binomial(link = "logit"))
+    eps.init <- fluctuation$coefficients[1:length(fluctuation$coefficients)])
     epss[1] <- eps.init
     ## get update
     new.f <- expit(logit(full) + eps.init*covar)
-    new.r <- SuperLearner::SuperLearner(Y = new.f, X = x[-s], family = gaussian(), SL.library = lib, ...)$SL.predict
+    ## update all of the reduced ones
+    for (i in 1:length(s)){
+        new.r <- SuperLearner::SuperLearner(Y = new.f, X = x[-s[[i]]], family = gaussian(), SL.library = lib, ...)$SL.predict    
+    }
+    
 
     ## now repeat until convergence
     if (eps.init == 0) {
@@ -54,7 +60,7 @@ variableImportanceTMLE <- function(full, reduced, y, x, s, lib, tol = .Machine$d
         eps <- eps.init
         k <- 1
         epss[k] <- eps
-        while(abs(eps) > tol & k < max.iter) {
+        while(abs(eps) > tol & k < max_iter) {
             ## if we didn't change by tol, break
             if (k > 1) {
                 if (abs(epss[k] - epss[k-1]) < tol) {
