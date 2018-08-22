0696146ce2f4d2d73d09176d4ddc1b83ee225372
Brian Williamson <brianw26@uw.edu>
2018-08-22 16:50:31 -0700

    fixing typos, making cv work with donsker class condition removed

--- R/cv_vim.R
+++ R/cv_vim.R
@@ -6,17 +6,16 @@
 #'
 #' @param Y the outcome.
 #' @param X the covariates. 
-#' @param f1 the fitted values from a flexible estimation technique regressing Y on X; a list of length V.
-#' @param f2 the fitted values from a flexible estimation technique regressing the fitted values in \code{f1} on X withholding the columns in \code{indx}; a list of length V.
+#' @param f1 the fitted values from a flexible estimation technique regressing Y on X; a list of length V, where each object is a list of two sets of predictions: the first on a first validation set, and the second on a second validation set.
+#' @param f2 the fitted values from a flexible estimation technique regressing the fitted values in \code{f1} on X withholding the columns in \code{indx}; a list of length V, where each object is a list of two sets of predictions: the first on a first validation set, and the second on a second validation set.
 #' @param indx the indices of the covariate(s) to calculate variable importance for; defaults to 1.
-#' @param V the number of folds for cross-validation, defaults to 10
-#' @param folds the folds to use, if f1 and f2 are supplied
-#' @param type the type of parameter (e.g., ANOVA-based is \code{"regression"})
+#' @param V the number of folds for cross-validation, defaults to 10.
+#' @param folds the folds to use, if f1 and f2 are supplied; an n by V matrix populated with 0s, 1s, and 2s, as returned by \code{\link[vimp]{two_validation_set_cv}}.
+#' @param type the type of parameter (e.g., ANOVA-based is \code{"regression"}).
 #' @param run_regression if outcome Y and covariates X are passed to \code{vimp_regression}, and \code{run_regression} is \code{TRUE}, then Super Learner will be used; otherwise, variable importance will be computed using the inputted fitted values. 
 #' @param SL.library a character vector of learners to pass to \code{SuperLearner}, if \code{f1} and \code{f2} are Y and X, respectively. Defaults to \code{SL.glmnet}, \code{SL.xgboost}, and \code{SL.mean}.
 #' @param alpha the level to compute the confidence interval at. Defaults to 0.05, corresponding to a 95\% confidence interval.
 #' @param na.rm should we remove NA's in the outcome and fitted values in computation? (defaults to \code{FALSE})
-#' @param update_denom logical; was smoothing used to estimate the denominator of the parameter of interest, if standardized? (defaults to \code{TRUE})
 #' @param ... other arguments to the estimation tool, see "See also".
 #'
 #' @return An object of class \code{vim}. See Details for more information.
@@ -80,20 +79,29 @@ cv_vim <- function(Y, X, f1, f2, indx = 1, V = 10, folds = NULL, type = "regress
   ## if formula, fit Super Learner with the given library
   if (run_regression) {
     ## set up the cross-validation
-    nums <- rep(seq_len(V), length = length(Y))
-    folds <- sample(nums)
-
+    folds <- two_validation_set_cv(length(Y), V)
     ## fit the super learner on each full/reduced pair
     fhat_ful <- list()
     fhat_red <- list()
     for (v in 1:V) {
+        ## set up the lists
+        fhat_ful[[v]] <- list()
+        fhat_red[[v]] <- list()
         ## fit super learner
-        fit <- SuperLearner::SuperLearner(Y = Y[folds != v, , drop = FALSE],
-         X = X[folds != v, , drop = FALSE], SL.library = SL.library, ...)
-        fhat_ful[[v]] <- SuperLearner::predict.SuperLearner(fit, newdata = X[folds == v, , drop = FALSE])$pred
-        red <- SuperLearner::SuperLearner(Y = fhat_ful[[v]],
-         X = X[folds != v, -indx, drop = FALSE], SL.library = SL.library, ...)
-        fhat_red[[v]] <- SuperLearner::predict.SuperLearner(red, newdata = X[folds == v, -indx, drop = FALSE])$pred
+        fit <- SuperLearner::SuperLearner(Y = Y[folds[, v] == 0, , drop = FALSE],
+         X = X[folds[, v] == 0, , drop = FALSE], SL.library = SL.library, ...)
+        fitted_v <- SuperLearner::predict.SuperLearner(fit)$pred
+        ## get predictions on the first validation fold
+        fhat_ful[[v]][[1]] <- SuperLearner::predict.SuperLearner(fit, newdata = X[folds[, v] == 1, , drop = FALSE])$pred
+        ## get predictions on the second validation fold
+        fhat_ful[[v]][[2]] <- SuperLearner::predict.SuperLearner(fit, newdata = X[folds[, v] == 2, , drop = FALSE])$pred
+        ## fit the super learner on the reduced covariates
+        red <- SuperLearner::SuperLearner(Y = fitted_v,
+         X = X[folds[, v] == 0, -indx, drop = FALSE], SL.library = SL.library, ...)
+        ## get predictions on the first validation fold
+        fhat_red[[v]][[1]] <- SuperLearner::predict.SuperLearner(red, newdata = X[folds[, v] == 1, -indx, drop = FALSE])$pred
+        ## get predictions on the second validation fold
+        fhat_red[[v]][[2]] <- SuperLearner::predict.SuperLearner(red, newdata = X[folds[, v] == 2, -indx, drop = FALSE])$pred
     }
     full <- reduced <- NA
 
@@ -101,10 +109,13 @@ cv_vim <- function(Y, X, f1, f2, indx = 1, V = 10, folds = NULL, type = "regress
 
     ## check to make sure they are the same length as y
     if (is.null(Y)) stop("Y must be entered.")
-    if (length(f1) != length(Y)) stop("The number of folds from the full regression must be the same length as the number of folds in y.")
-    if (length(f2) != length(Y)) stop("The number of folds from the reduced regression must be the same length as the number of folds in y.")
+    if (is.null(f1)) stop("You must specify a list of lists of predicted values from a regression of Y on X; the two sets of predictions come from using two validation sets.")
+    if (is.null(f2)) stop("You must specify a list of lists of predicted values from a regression of the fitted values from the Y on X regression on the reduced set of covariates; the two sets of predictions come from using two validation sets.")
+    if (is.null(folds)) stop("You must specify an n by V matrix of folds; this is computed, e.g., by two_validation_set_cv().")
+    if (length(f1) != length(V)) stop("The number of folds from the full regression must be the same length as the number of folds.")
+    if (length(f2) != length(V)) stop("The number of folds from the reduced regression must be the same length as the number of folds.")
 
-    ## set up the fitted value objects (both are lists!)
+    ## set up the fitted value objects (both are lists of lists!)
     fhat_ful <- f1
     fhat_red <- f2
 
@@ -117,23 +128,23 @@ cv_vim <- function(Y, X, f1, f2, indx = 1, V = 10, folds = NULL, type = "regress
   updates <- vector("numeric", V)
   ses <- vector("numeric", V)
   for (v in 1:V) {
-    naive_cv[v] <- onestep_based_estimator(fhat_ful[[v]], fhat_red[[v]], Y[folds == v, ], type = type, na.rm = na.rm)
+    naive_cv[v] <- onestep_based_estimator(fhat_ful[[v]][[1]], fhat_red[[v]][[1]], Y[folds[, v] == 1, ], type = type, na.rm = na.rm)
     
-    if (update_denom) { ## here, use the IC of the full standardized parameter
-      updates[v] <- mean(vimp_update(fhat_ful[[v]], fhat_red[[v]], Y[folds == v, ], type = type, na.rm = na.rm), na.rm = na.rm)
-      ses[v] <- mean(vimp_update(fhat_ful[[v]], fhat_red[[v]], Y[folds == v, ], type = type, na.rm = na.rm)^2, na.rm = na.rm) 
-    } else { ## here, use the fact that the unstandardized and variance are jointly normal, along with the delta method
-      ## naive estimators of numerator (based on subset, due to smoothing), denominator (based on all data, no smoothing)
-      naive.j <- mean((fhat_ful[[v]] - fhat_red[[v]]) ^ 2, na.rm = na.rm)
-      naive.var <- mean((unlist(Y) - mean(unlist(Y), na.rm = na.rm))^2, na.rm = na.rm)
-      ## influence curves
-      contrib.denom <- ((unlist(Y) - mean(unlist(Y), na.rm = na.rm))^2 - naive.var)
-      contrib.num <- vimp_update(fhat_ful[[v]], fhat_red[[v]], Y[folds == v, , drop = FALSE], na.rm = na.rm)
-      ## update
-      updates[v] <- (mean(contrib.num, na.rm = na.rm) - mean(contrib.denom, na.rm = na.rm))/naive.var
-      ## standard deviation, based on delta method
-      ses[v] <- sqrt(mean((1/naive.var^2)*contrib.num^2, na.rm = na.rm) + mean((naive.j/(naive.var)^2)^2*contrib.denom^2, na.rm = na.rm))
-    }
+    # if (update_denom) { ## here, use the IC of the full standardized parameter
+    updates[v] <- mean(vimp_update(fhat_ful[[v]][[2]], fhat_red[[v]][[2]], Y[folds[, v] == 2, ], type = type, na.rm = na.rm), na.rm = na.rm)
+    ses[v] <- mean(vimp_update(fhat_ful[[v]][[2]], fhat_red[[v]][[2]], Y[folds[, v] == 2, ], type = type, na.rm = na.rm)^2, na.rm = na.rm) 
+    # } else { ## here, use the fact that the unstandardized and variance are jointly normal, along with the delta method
+    #   ## naive estimators of numerator (based on subset, due to smoothing), denominator (based on all data, no smoothing)
+    #   naive.j <- mean((fhat_ful[[v]][[1]] - fhat_red[[v]][[1]]) ^ 2, na.rm = na.rm)
+    #   naive.var <- mean((unlist(Y) - mean(unlist(Y), na.rm = na.rm))^2, na.rm = na.rm)
+    #   ## influence curves
+    #   contrib.denom <- ((unlist(Y) - mean(unlist(Y), na.rm = na.rm))^2 - naive.var)
+    #   contrib.num <- 2*(Y[folds[, v] == 2, , drop = FALSE] - fhat_ful[[v]][[2]])*(fhat_ful[[v]][[2]] - fhat_red[[v]][[2]])
+    #   ## update
+    #   updates[v] <- (mean(contrib.num, na.rm = na.rm) - mean(contrib.denom, na.rm = na.rm))/naive.var
+    #   ## standard deviation, based on delta method
+    #   ses[v] <- sqrt(mean((1/naive.var^2)*contrib.num^2, na.rm = na.rm) + mean((naive.j/(naive.var)^2)^2*contrib.denom^2, na.rm = na.rm))
+    # }
       
   }
   est <- mean(naive_cv) + mean(updates)
