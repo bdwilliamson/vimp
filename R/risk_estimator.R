8240a323385e77e7746464c952738a7cb1af0f89
Brian Williamson <brianw26@uw.edu>
2019-03-18 12:04:29 -0700

    estimator and ic estimator for single risk

---
+++ R/risk_estimator.R
@@ -0,0 +1,73 @@
+#' Estimate a nonparametric risk functional
+#'
+#' Compute nonparametric estimates of the chosen risk functional.
+#'
+#' @param fitted_values fitted values from a regression function.
+#' @param y the outcome.
+#' @param type which parameter are you estimating (defaults to \code{anova}, for ANOVA-based variable importance)?
+#' @param na.rm logical; should NA's be removed in computation? (defaults to \code{FALSE})
+#'
+#' @return The estimated variable importance for the given group of left-out covariates.
+#'
+#' @details See the paper by Williamson, Gilbert, Simon, and Carone for more
+#' details on the mathematics behind this function and the definition of the parameter of interest.
+#' @export
+risk_estimator <- function(fitted_values, y, type = "anova", na.rm = FALSE) {
+
+  ## first calculate the naive
+  if (type == "regression" | type == "anova") {
+    naive <- mean((full - reduced) ^ 2, na.rm = na.rm)/mean((y - mean(y, na.rm = na.rm)) ^ 2, na.rm = na.rm)
+  } else if (type == "deviance") { 
+    if (is.null(dim(y))) { # assume that zero is in first column
+        y_mult <- cbind(1 - y, y)
+    } else {
+        y_mult <- y
+    }
+    if (is.null(dim(full))) { # assume predicting y = 1
+      full_mat <- cbind(1 - full, full)
+      reduced_mat <- cbind(1 - reduced, reduced)
+    } else if(dim(full)[2] < 2) {
+        full_mat <- cbind(1 - full, full)
+        reduced_mat <- cbind(1 - reduced, reduced)
+    } else {
+        full_mat <- full
+        reduced_mat <- reduced
+    }
+    p <- apply(y_mult, 2, mean)
+    naive_num <- 2*sum(diag(t(y_mult)%*%log(full_mat/reduced_mat)), na.rm = na.rm)/dim(y_mult)[1]
+    naive_denom <- -1*sum(log(p))
+    naive <- naive_num/naive_denom
+  } else if (type == "r_squared"){
+    denom <- mean((y - mean(y, na.rm = na.rm))^2, na.rm = na.rm)
+    mse_full <- mean((y - full)^2, na.rm = na.rm)/denom
+    mse_reduced <- mean((y - reduced)^2, na.rm = na.rm)/denom
+    naive <- (1 - mse_full) - (1 - mse_reduced)
+  } else if (type == "auc") {
+    full_pred <- ROCR::prediction(predictions = full, labels = y)
+    red_pred <- ROCR::prediction(predictions = reduced, labels = y)
+
+    naive_auc_full <- unlist(ROCR::performance(prediction.obj = full_pred, measure = "auc", x.measure = "cutoff")@y.values)
+    naive_auc_reduced <- unlist(ROCR::performance(prediction.obj = red_pred, measure = "auc", x.measure = "cutoff")@y.values)
+    naive <- naive_auc_full - naive_auc_reduced
+  } else if (type == "accuracy") {
+    contrib_full <- mean((full > 1/2) != y)
+    contrib_reduced <- mean((reduced > 1/2) != y)
+    naive <- (1 - contrib_full) - (1 - contrib_reduced)
+  } else if (type == "avg_value") {
+    stop("We currently do not support the entered variable importance parameter.")
+  } else {
+    stop("We currently do not support the entered variable importance parameter.")
+  }
+
+  ## now add on the mean of the ic, only if type == "regression" or "anova"
+  if (type == "regression" | type == "anova") {
+    onestep <- naive + mean(vimp_update(full, reduced, y, type, na.rm = na.rm), na.rm = na.rm)  
+  } else {
+    onestep <- NA
+  }  
+
+  ## return
+  ret <- c(onestep, naive)
+  
+  return(ret)
+}
---
+++ R/risk_update.R
@@ -0,0 +1,82 @@
+#' Estimate the influence function for an estimator of risk
+#'
+#' Estimate the influence function for the given risk parameter.
+#'
+#' @param fitted_values fitted values from a regression function.
+#' @param y the outcome.
+#' @param type which risk parameter are you estimating (defaults to \code{r_squared}, for the $R^2$)?
+#' @param na.rm logical; should NAs be removed in computation? (defaults to \code{FALSE})
+#'
+#' @return The influence function values for the given group of left-out covariates.
+#'
+#' @details See the paper by Williamson, Gilbert, Simon, and Carone for more
+#' details on the mathematics behind this function and the definition of the parameter of interest.
+#'
+#' @export
+risk_update <- function(fitted_values, y, type = "r_squared", na.rm = FALSE) {
+  
+  ## calculate the necessary pieces for the influence curve
+  if (type == "regression" | type == "anova") {
+    stop("Hypothesis testing is not supported for this variable importance functional. Please use the 'difference in R-squared' functional instead.")
+  } else if (type == "deviance") {
+    if (is.null(dim(y))) { # assume that zero is in first column
+      y_mult <- cbind(1 - y, y)
+    } else {
+      y_mult <- y
+    }
+    if (is.null(dim(fitted_values))) { # assume predicting y = 1
+      fitted_mat <- cbind(1 - fitted_values, fitted_values)
+    } else if(dim(fitted_values)[2] < 2) {
+      fitted_mat <- cbind(1 - fitted_values, fitted_values)
+    } else {
+      fitted_mat <- fitted_values
+    }
+    p <- apply(y_mult, 2, mean)
+    naive_num <- 2*sum(diag(t(y_mult)%*%log(fitted_mat)), na.rm = na.rm)/dim(y_mult)[1]
+    naive_denom <- -1*sum(log(p))
+    d_risk <- 2*rowSums(y_mult*log(fitted_mat), na.rm = na.rm) - naive_num
+    ## influence function of the denominator
+    d_denom <- rowSums(-1/p*((y_mult == 1) - p))
+  } else if (type == "r_squared") {
+    naive_denom <- mean((y - mean(y, na.rm = na.rm))^2, na.rm = na.rm)
+    mse <- mean((y - fitted_values)^2, na.rm = na.rm)/naive_denom
+    
+    d <- (y - fitted_values)^2 - mse
+    d_denom <- (y - mean(y, na.rm = na.rm))^2 - naive_denom
+    d_risk <- (-1)*d 
+  } else if (type == "auc") {
+    p_0 <- mean(y == 0)
+    p_1 <- mean(y == 1)
+    
+    preds <- ROCR::prediction(predictions = fitted_values, labels = y)
+    
+    sens <- unlist(lapply(as.list(fitted_values), function(x) mean(fitted_values[y == 0] < x)))
+    spec <- unlist(lapply(as.list(fitted_values), function(x) mean(fitted_values[y == 1] > x)))
+        
+    contrib_1 <- (y == 1)/p_1*sens
+    contrib_0 <- (y == 0)/p_0*spec
+    naive_auc <- unlist(ROCR::performance(prediction.obj = preds, measure = "auc", x.measure = "cutoff")@y.values)
+    d_risk <- contrib_1 + contrib_0 - ((y == 0)/p_0 + (y == 1)/p_1)*naive_auc
+        
+    ic_update <- d_risk
+    
+  } else if (type == "accuracy") {
+    contrib <- mean((fitted_values > 1/2) != y)
+    d_risk <- ((fitted_values > 1/2) != y) - contrib
+    ic_update <- d_risk
+  } else {
+    stop("We currently do not support the entered variable importance parameter.")
+  }
+  
+  ## influence curve
+  if (type %in% c("regression", "anova", "r_squared", "deviance")) {
+    ic_update <- d_risk/naive_denom - naive_num/(naive_denom ^ 2)*d_denom    
+  } else if (type %in% c("auc", "accuracy")) {
+    # already computed it above
+  } else {
+    stop("We currently do not support the entered variable importance parameter.")
+  }
+  
+  
+  return(ic_update)
+}
