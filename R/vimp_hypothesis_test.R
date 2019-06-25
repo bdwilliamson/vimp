472cc40df00b065f7a6cf6d03985e9d201216b90
Brian Williamson <brianw26@uw.edu>
2019-06-25 16:12:22 -0700

    update all to predictiveness

--- R/vimp_hypothesis_test.R
+++ R/vimp_hypothesis_test.R
@@ -1,7 +1,7 @@
 #' Perform a hypothesis test against the null hypothesis of zero importance
 #'
 #' Perform a hypothesis test against the null hypothesis of zero importance by: 
-#' (i) for a user-specified level \eqn{\alpha}, compute a \eqn{(1 - \alpha)\times 100}\% confidence interval around the risk for both the full and reduced regression functions (these must be estimated on independent splits of the data);
+#' (i) for a user-specified level \eqn{\alpha}, compute a \eqn{(1 - \alpha)\times 100}\% confidence interval around the predictiveness for both the full and reduced regression functions (these must be estimated on independent splits of the data);
 #' (ii) if the intervals do not overlap, reject the null hypothesis.
 #'
 #' @param full either (i) fitted values from a regression of the outcome on the full set of covariates from a first independent split of the data (if \code{cv = FALSE}) or (ii) a list of predicted values from a cross-validated procedure (if \code{cv = TRUE}).
@@ -11,7 +11,7 @@
 #' @param weights weights for the computed influence curve (e.g., inverse probability weights for coarsened-at-random settings)
 #' @param type which parameter are you estimating (defaults to \code{r_squared}, for difference in R-squared-based variable importance)?
 #' @param alpha the desired type I error rate (defaults to 0.05).
-#' @param cv was V-fold cross-validation used to estimate the risks (\code{TRUE}) or was the sample split in two (\code{FALSE}); defaults to \code{FALSE}.
+#' @param cv was V-fold cross-validation used to estimate the predictiveness (\code{TRUE}) or was the sample split in two (\code{FALSE}); defaults to \code{FALSE}.
 #' @param na.rm logical; should NAs be removed in computation? (defaults to \code{FALSE})
 #'
 #' @return \code{TRUE} if the null hypothesis is rejected (i.e., if the confidence intervals do not overlap); otherwise, \code{FALSE}.
@@ -29,27 +29,27 @@ vimp_hypothesis_test <- function(full, reduced, y, folds, weights = rep(1, lengt
     if (!cv) {
         fold_nums <- unique(folds)
         ## point estimates of the risk
-        risk_full <- predictiveness_point_est(fitted_values = full, y = y[folds == fold_nums[1]], type = type, na.rm = na.rm)
-        risk_reduced <- predictiveness_point_est(fitted_values = reduced, y = y[folds == fold_nums[2]], type = type, na.rm = na.rm)
+        predictiveness_full <- predictiveness_point_est(fitted_values = full, y = y[folds == fold_nums[1]], type = type, na.rm = na.rm)
+        predictiveness_redu <- predictiveness_point_est(fitted_values = reduced, y = y[folds == fold_nums[2]], type = type, na.rm = na.rm)
 
         ## influence curve estimates for the risk, and corresponding ses
-        ic_full <- risk_update(fitted_values = full, y = y[folds == fold_nums[1]], weights = weights, type = type, na.rm = na.rm)
-        ic_reduced <- risk_update(fitted_values = reduced, y = y[folds == fold_nums[2]], weights = weights, type = type, na.rm = na.rm)
+        ic_full <- predictiveness_update(fitted_values = full, y = y[folds == fold_nums[1]], weights = weights, type = type, na.rm = na.rm)
+        ic_redu <- predictiveness_update(fitted_values = reduced, y = y[folds == fold_nums[2]], weights = weights, type = type, na.rm = na.rm)
         se_full <- vimp_se(ic_full, na.rm = na.rm)
-        se_reduced <- vimp_se(ic_full, na.rm = na.rm)
-        ## CIs for both risks
-        risk_ci_full <- vimp_ci(est = risk_full, se = vimp_se(ic_full, na.rm = na.rm), level = 1 - alpha)
-        risk_ci_reduced <- vimp_ci(est = risk_reduced, se = vimp_se(ic_reduced, na.rm = na.rm), level = 1 - alpha)
+        se_redu <- vimp_se(ic_full, na.rm = na.rm)
+        ## CIs for both predictivenesses
+        predictiveness_ci_full <- vimp_ci(est = predictiveness_full, se = vimp_se(ic_full, na.rm = na.rm), level = 1 - alpha)
+        predictiveness_ci_reduced <- vimp_ci(est = predictiveness_redu, se = vimp_se(ic_redu, na.rm = na.rm), level = 1 - alpha)
         
         ## hypothesis test (check that lower bound of full is bigger than upper bound of reduced)
         ## (since measures are R^2 [bigger = better], auc [bigger = better], accuracy [bigger = better])
-        hyp_test <- risk_ci_full[1] > risk_ci_reduced[2]
+        hyp_test <- predictiveness_ci_full[1] > predictiveness_ci_reduced[2]
         
         ## to get a p-value, apply the CIs to a range of levels; p-value is the largest at which we would still reject
         levels <- seq(0.0001, 1 - 0.0001, 0.0001)
-        risk_cis_full <- t(apply(matrix(1 - levels), 1, function(x) vimp_ci(est = risk_full, se = risk_se_full, level = x)))
-        risk_cis_redu <- t(apply(matrix(1 - levels), 1, function(x) vimp_ci(est = risk_reduced, se = risk_se_reduced, level = x)))
-        hyp_tests <- risk_cis_full[, 1] > risk_cis_redu[, 2]
+        predictiveness_cis_full <- t(apply(matrix(1 - levels), 1, function(x) vimp_ci(est = predictiveness_full, se = predictiveness_se_full, level = x)))
+        predictiveness_cis_redu <- t(apply(matrix(1 - levels), 1, function(x) vimp_ci(est = predictiveness_redu, se = predictiveness_se_redu, level = x)))
+        hyp_tests <- predictiveness_cis_full[, 1] > predictiveness_cis_redu[, 2]
         if (all(hyp_tests)) {
           p_value <- 0.0001
         } else if (!any(hyp_tests)) {
@@ -58,37 +58,37 @@ vimp_hypothesis_test <- function(full, reduced, y, folds, weights = rep(1, lengt
           p_value <- min(levels[hyp_tests])
         } 
         
-    } else { ## V-fold CV; compute a p-value comparing risk on each fold v = 1,...,V with reduced ones, average
+    } else { ## V-fold CV; compute a p-value comparing predictiveness on each fold v = 1,...,V with reduced ones, average
              ## testing decision is based on average p-value
         V <- length(unique(folds))
         p_values <- vector("list", length = V)
-        risks_full <- vector("numeric", length = V)
-        risks_reduced <- vector("numeric", length = V)
-        risk_vars_full <- vector("numeric", length = V)
-        risk_vars_reduced <- vector("numeric", length = V)
+        predictiveness_fulls <- vector("numeric", length = V)
+        predictiveness_redus <- vector("numeric", length = V)
+        predictiveness_vars_full <- vector("numeric", length = V)
+        predictiveness_vars_redu <- vector("numeric", length = V)
         for (v in 1:V) {
-          ## compute full risk on fold v, reduced risks on folds -v; also variances
-          risk_full <- predictiveness_point_est(fitted_values = full[[v]], y = y[folds == v], type = type, na.rm = na.rm)
-          risk_var_full <- mean(risk_update(fitted_values = full[[v]], y = y[folds == v], weights = weights[folds == v], type = type, 
+          ## compute full predictiveness on fold v, reduced predictivenesses on folds -v; also variances
+          predictiveness_full <- predictiveness_point_est(fitted_values = full[[v]], y = y[folds == v], type = type, na.rm = na.rm)
+          predictiveness_var_full <- mean(predictiveness_update(fitted_values = full[[v]], y = y[folds == v], weights = weights[folds == v], type = type, 
                                             na.rm = na.rm)^2)
-          risk_se_full <- sqrt(risk_var_full/length(y[folds == v]))
-          risks_reduced <- sapply(seq_len(V)[-v], function(x) predictiveness_point_est(fitted_values = reduced[[x]],
+          predictiveness_se_full <- sqrt(predictiveness_var_full/length(y[folds == v]))
+          predictiveness_redus <- sapply(seq_len(V)[-v], function(x) predictiveness_point_est(fitted_values = reduced[[x]],
                                                                              y = y[folds == x], type = type,
                                                                              na.rm = na.rm))
-          risk_vars_reduced <- sapply(seq_len(V)[-v], function(x) mean(risk_update(fitted_values = reduced[[x]],
+          predictiveness_vars_redu <- sapply(seq_len(V)[-v], function(x) mean(predictiveness_update(fitted_values = reduced[[x]],
                                                                                  y = y[folds == x], weights = weights[folds == v], type = type,
                                                                                  na.rm = na.rm)^2))
-          risk_ses_reduced <- sapply(1:length(seq_len(V)[-v]), function(x) sqrt(risk_vars_reduced[x]/length(y[folds == x])))
+          predictiveness_ses_redu <- sapply(1:length(seq_len(V)[-v]), function(x) sqrt(predictiveness_vars_reduced[x]/length(y[folds == x])))
 
           ## compute a p-value
           levels <- seq(0.0001, 1 - 0.0001, 0.0001)
-          ## get the lower limit of the full risk CI; vector of length(levels)
-          risk_cis_ll_full <- apply(matrix(1 - levels), 1, function(x) vimp_ci(est = risk_full, se = risk_se_full, level = x)[, 1])
-          ## get the upper limit of the reduced risk CI for each fold; dimension length(levels) by V - 1 
-          risk_cis_ul_redu <- t(apply(matrix(1 - levels), 1, function(x) sapply(1:(V - 1), function(v) vimp_ci(est = risks_reduced[v], se = risk_ses_reduced[v],
+          ## get the lower limit of the full predictiveness CI; vector of length(levels)
+          predictiveness_cis_ll_full <- apply(matrix(1 - levels), 1, function(x) vimp_ci(est = predictiveness_full, se = predictiveness_se_full, level = x)[, 1])
+          ## get the upper limit of the reduced predictiveness CI for each fold; dimension length(levels) by V - 1 
+          predictiveness_cis_ul_redu <- t(apply(matrix(1 - levels), 1, function(x) sapply(1:(V - 1), function(v) vimp_ci(est = predictivenesss_reduced[v], se = predictiveness_ses_reduced[v],
                                                                                                             level = x)[, 2])))
-          ## compare lower limit of full risk CI to all upper limits of reduced risk cis
-          hyp_tests <- t(apply(matrix(1:dim(risk_cis_ul_redu)[1]), 1, function(x) risk_cis_ll_full[x] > risk_cis_ul_redu[x, ]))
+          ## compare lower limit of full predictiveness CI to all upper limits of reduced predictiveness cis
+          hyp_tests <- t(apply(matrix(1:dim(predictiveness_cis_ul_redu)[1]), 1, function(x) predictiveness_cis_ll_full[x] > predictiveness_cis_ul_redu[x, ]))
           ## compute a p-value for each fold
           check_fold_hyp_tests <- function(fold_hyp_tests) {
             if (all(fold_hyp_tests)) {
@@ -102,23 +102,23 @@ vimp_hypothesis_test <- function(full, reduced, y, folds, weights = rep(1, lengt
           }
           ## save off the p-value
           p_values[[v]] <- mean(apply(hyp_tests, 2, check_fold_hyp_tests))
-          ## save off the risks and the variances
-          risks_full[v] <- risk_full
-          risk_vars_full[v] <- risk_var_full
-          risks_reduced[v] <- predictiveness_point_est(fitted_values = reduced[[v]], y = y[folds == v], type = type, na.rm = na.rm)
-          risk_vars_reduced[v] <- mean(risk_update(fitted_values = reduced[[v]], y = y[folds == v], weights = weights, type = type, 
+          ## save off the predictivenesses and the variances
+          predictiveness_fulls[v] <- predictiveness_full
+          predictiveness_vars_full[v] <- predictiveness_var_full
+          predictiveness_redus[v] <- predictiveness_point_est(fitted_values = reduced[[v]], y = y[folds == v], type = type, na.rm = na.rm)
+          predictiveness_vars_redu[v] <- mean(predictiveness_update(fitted_values = reduced[[v]], y = y[folds == v], weights = weights, type = type, 
                                                    na.rm = na.rm)^2)
         }
         ## average the p-values, make a testing decision
         p_value <- mean(unlist(p_values))
         hyp_test <- p_value < alpha
-        ## average the full and reduced risks, get a CI
-        risk_full <- mean(risks_full)
-        risk_reduced <- mean(risks_reduced)
-        risk_ci_full <- vimp_ci(est = risk_full, se = sqrt(mean(risk_vars_full)/length(y)), level = 1 - alpha)
-        risk_ci_reduced <- vimp_ci(est = risk_reduced, se = sqrt(mean(risk_vars_reduced)/length(y)), level = 1 - alpha)
+        ## average the full and reduced predictivenesses, get a CI
+        predictiveness_full <- mean(predictiveness_fulls)
+        predictiveness_redu <- mean(predictiveness_redus)
+        predictiveness_ci_full <- vimp_ci(est = predictiveness_full, se = sqrt(mean(predictiveness_vars_full)/length(y)), level = 1 - alpha)
+        predictiveness_ci_reduced <- vimp_ci(est = predictiveness_redu, se = sqrt(mean(predictiveness_vars_reduced)/length(y)), level = 1 - alpha)
     }
   }
-  return(list(test = hyp_test, p_value = p_value, risk_full = risk_full, risk_reduced = risk_reduced,
-              risk_ci_full = risk_ci_full, risk_ci_reduced = risk_ci_reduced))
+  return(list(test = hyp_test, p_value = p_value, predictiveness_full = predictiveness_full, predictiveness_redu = predictiveness_redu,
+              predictiveness_ci_full = predictiveness_ci_full, predictiveness_ci_reduced = predictiveness_ci_reduced))
 }
