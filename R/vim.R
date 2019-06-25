1bfe22172afa804cc8bd79c0b157ed699f1aa6ae
Brian Williamson <brianw26@uw.edu>
2019-06-25 12:33:33 -0700

    get up to hyp testing with new reorg

--- R/vim.R
+++ R/vim.R
@@ -84,123 +84,158 @@
 
 vim <- function(Y, X, f1 = NULL, f2 = NULL, indx = 1, weights = rep(1, length(Y)), type = "r_squared", run_regression = TRUE, SL.library = c("SL.glmnet", "SL.xgboost", "SL.mean"), alpha = 0.05, na.rm = FALSE, 
                           f1_split = NULL, f2_split = NULL, folds = NULL, ...) {
-  ## check to see if f1 and f2 are missing
-  ## if the data is missing, stop and throw an error
-  if (missing(f1) & missing(Y)) stop("You must enter either Y or fitted values for the full regression.")
-  if (missing(f2) & missing(Y)) stop("You must enter either Y or fitted values for the reduced regression.")
+    ## check to see if f1 and f2 are missing
+    ## if the data is missing, stop and throw an error
+    if (missing(f1) & missing(Y)) stop("You must enter either Y or fitted values for the full regression.")
+    if (missing(f2) & missing(Y)) stop("You must enter either Y or fitted values for the reduced regression.")
   
-  ## if run_regression = TRUE, then fit SuperLearner
-  if (run_regression) {
+    ## get the correct measure function; if not one of the supported ones, say so
+    types <- c("accuracy", "auc", "deviance", "r_squared", "anova")
+    full_type <- pmatch(type, types)
+    if (is.na(full_type)) stop("We currently do not support the entered variable importance parameter.")
+
+    ## if run_regression = TRUE, then fit SuperLearner
+    if (run_regression) {
     
-    ## if formula is entered, need a library for Super Learner
-    if (is.null(SL.library)) stop("You must enter a library of learners for the Super Learner.")
+        ## if formula is entered, need a library for Super Learner
+        if (is.null(SL.library)) stop("You must enter a library of learners for the Super Learner.")
     
-    ## set up the reduced X
-    X_minus_s <- X[, -indx, drop = FALSE]
+        ## set up the reduced X
+        X_minus_s <- X[, -indx, drop = FALSE]
     
-    ## fit the Super Learner given the specified library
-    full <- SuperLearner::SuperLearner(Y = Y, X = X, SL.library = SL.library, ...)
+        ## fit the Super Learner given the specified library
+        full <- SuperLearner::SuperLearner(Y = Y, X = X, SL.library = SL.library, ...)
     
-    ## get the fitted values
-    fhat_ful <- SuperLearner::predict.SuperLearner(full)$pred
+        ## get the fitted values
+        fhat_ful <- SuperLearner::predict.SuperLearner(full)$pred
     
-    ## fit the super learner on the reduced covariates:
-    ## always use gaussian; if first regression was mean, use Y instead
-    arg_lst <- list(...)
-    if (type == "r_squared" | type == "anova") {
-      if (length(unique(fhat_ful)) == 1) {
-        arg_lst$Y <- Y
-      } else {
-        arg_lst$family <- stats::gaussian()
-        arg_lst$Y <- fhat_ful 
-      }
-      arg_lst$X <- X_minus_s
-      arg_lst$SL.library <- SL.library
-    }
-    reduced <- do.call(SuperLearner::SuperLearner, arg_lst)  
+        ## fit the super learner on the reduced covariates:
+        ## always use gaussian; if first regression was mean, use Y instead
+        arg_lst <- list(...)
+        if (full_type == "r_squared" | full_type == "anova") {
+            if (length(unique(fhat_ful)) == 1) {
+                arg_lst$Y <- Y
+            } else {
+                arg_lst$family <- stats::gaussian()
+                arg_lst$Y <- fhat_ful 
+            }
+            arg_lst$X <- X_minus_s
+            arg_lst$SL.library <- SL.library
+        }
+        reduced <- do.call(SuperLearner::SuperLearner, arg_lst)  
     
-    ## get the fitted values
-    fhat_red <- SuperLearner::predict.SuperLearner(reduced)$pred
+        ## get the fitted values
+        fhat_red <- SuperLearner::predict.SuperLearner(reduced)$pred
     
-    ## get the fitted values on splits for hypothesis testing; not if type == "anova" 
-    if (type != "anova") {
-      folds <- sample(1:2, length(fhat_ful), replace = TRUE, prob = c(0.5, 0.5))
-      split_full <- SuperLearner::SuperLearner(Y = Y[folds == 1], X = X[folds == 1, , drop = FALSE], SL.library = SL.library, ...)
-      split_reduced <- SuperLearner::SuperLearner(Y = Y[folds == 2], X = X_minus_s[folds == 2, , drop = FALSE], SL.library = SL.library, ...)
+        ## get the fitted values on splits for hypothesis testing; not if full_type == "anova" 
+        if (full_type != "anova") {
+            folds <- sample(1:2, length(fhat_ful), replace = TRUE, prob = c(0.5, 0.5))
+            split_full <- SuperLearner::SuperLearner(Y = Y[folds == 1], X = X[folds == 1, , drop = FALSE], SL.library = SL.library, ...)
+            split_reduced <- SuperLearner::SuperLearner(Y = Y[folds == 2], X = X_minus_s[folds == 2, , drop = FALSE], SL.library = SL.library, ...)
       
-      fhat_split_ful <- SuperLearner::predict.SuperLearner(split_full)$pred
-      fhat_split_red <- SuperLearner::predict.SuperLearner(split_reduced)$pred  
+            fhat_split_ful <- SuperLearner::predict.SuperLearner(split_full)$pred
+            fhat_split_red <- SuperLearner::predict.SuperLearner(split_reduced)$pred  
+        } else {
+            folds <- NA
+            split_full <- NA
+            split_reduced <- NA
+      
+            fhat_split_ful <- NA
+            fhat_split_red <- NA
+        }
+    } else { ## otherwise they are fitted values
+    
+        ## check to make sure they are the same length as y
+        if (is.null(Y)) stop("Y must be entered.")
+        if (length(f1) != length(Y)) stop("Fitted values from the full regression must be the same length as Y.")
+        if (length(f2) != length(Y)) stop("Fitted values from the reduced regression must be the same length as Y.")
+        ## if f1_split or f2_split are not entered, don't do a hypothesis test; print a warning
+        if (full_type != "anova" & (is.null(f1_split) | is.null(f2_split))) warning("Fitted values from independent data splits must be entered to perform a hypothesis test; hypothesis testing not done.")
+    
+        ## set up the fitted value objects
+        fhat_ful <- f1
+        fhat_red <- f2
+    
+        full <- reduced <- NA    
+    
+        ## get the hypothesis testing objects
+        split_full <- split_reduced <- NA
+        fhat_split_ful <- f1_split
+        fhat_split_red <- f2_split
+    }
+  
+    ## calculate the estimators 
+    ests <- vimp_point_est(fhat_ful, fhat_red, Y, weights = weights, type = full_type, na.rm = na.rm)
+  
+    ## if type = "anova", then use corrected; else use plug-in
+    if (full_type == "anova") {
+        est <- ests[1]
+        naive <- ests[2]
+        risk_full <- NA
+        risk_reduced <- NA
+        risk_ci_full <- NA
+        risk_ci_reduced <- NA
     } else {
-      folds <- NA
-      split_full <- NA
-      split_reduced <- NA
-      
-      fhat_split_ful <- NA
-      fhat_split_red <- NA
+        est <- ests[2]
+        naive <- NA
+        risk_full <- risk_estimator(fhat_ful, Y, type = full_type, na.rm = na.rm)
+        risk_reduced <- risk_estimator(fhat_red, Y, type = full_type, na.rm = na.rm)
+        risk_ci_full <- vimp_ci(risk_full, se = vimp_se(risk_update(fhat_ful, Y, weights = weights, type = full_type, na.rm = na.rm)), level = 1 - alpha)
+        risk_ci_reduced <- vimp_ci(risk_reduced, se = vimp_se(risk_update(fhat_red, Y, weights = weights, type = full_type, na.rm = na.rm)), level = 1 - alpha)
     }
+    ## compute the update
+    update <- vimp_update(fhat_ful, fhat_red, Y, weights = weights, type = full_type, na.rm = na.rm)
+  
+    if (full_type == "regression" | full_type == "anova" | full_type == "r_squared") {
+        denom_point_est <- mean((Y - mean(Y))^2, na.rm = na.rm) 
+        denom_ic <- (Y - mean(Y, na.rm = na.rm))^2 - denom_point_est
+    } else if (full_type == "deviance") {
+        if (is.null(dim(y))) { # assume that zero is in first column
+            y_mult <- cbind(1 - Y, Y)
+        } else {
+            y_mult <- Y
+        }
+        p <- apply(y_mult, 2, mean, na.rm = na.rm)
+        denom_point_est <- (-1)*sum(log())
+        denom_ic <- rowSums(-1/p*((y_mult == 1) - p))
+    } else {
+        denom_point_est <- NA
+        denom_ic <- NA
+    }
+    denom <- list(point_est = denom_point_est, ic = denom_ic)
     
-    
-  } else { ## otherwise they are fitted values
-    
-    ## check to make sure they are the same length as y
-    if (is.null(Y)) stop("Y must be entered.")
-    if (length(f1) != length(Y)) stop("Fitted values from the full regression must be the same length as Y.")
-    if (length(f2) != length(Y)) stop("Fitted values from the reduced regression must be the same length as Y.")
-    ## if f1_split or f2_split are not entered, don't do a hypothesis test; print a warning
-    if (type != "anova" & (is.null(f1_split) | is.null(f2_split))) warning("Fitted values from independent data splits must be entered to perform a hypothesis test; hypothesis testing not done.")
-    
-    ## set up the fitted value objects
-    fhat_ful <- f1
-    fhat_red <- f2
-    
-    full <- reduced <- NA    
-    
-    ## get the hypothesis testing objects
-    split_full <- split_reduced <- NA
-    fhat_split_ful <- f1_split
-    fhat_split_red <- f2_split
-  }
+    ## compute the standard error, using the numerator (if we have a denominator)
+    se <- vimp_se(est, update, denom, scale = "log", na.rm = na.rm)
   
-  ## calculate the estimators 
-  ests <- vimp_point_est(fhat_ful, fhat_red, Y, weights = weights, type = type, na.rm = na.rm)
+    ## at this point, change to scaled MSE or deviance
+    if (full_type == "r_squared" | full_type == "deviance") {
+        tmp <- est/denom_point_est
+        tmp_update <- matrix(c(1/denom$point_est, -est/(denom$point_est^2)), nrow = 1)%*%cbind(update, denom$ic)
+        est <- tmp
+        update <- tmp_update
+    }
+    ## compute the confidence interval
+    ci <- vimp_ci(est, se, scale = "log", level = 1 - alpha)
+    ## if r^2, at this point convert to R^2
+    if (full_type == "r_squared") {
+        tmp <- 1 - est
+        tmp_update <- (-1)*update
+        est <- tmp
+        update <- tmp_update
+    }
   
-  ## if type = "anova", then use onestep; else use naive
-  if (type == "anova") {
-    est <- ests[1]
-    naive <- ests[2]
-    risk_full <- NA
-    risk_reduced <- NA
-    risk_ci_full <- NA
-    risk_ci_reduced <- NA
-  } else {
-    est <- ests[2]
-    naive <- NA
-    risk_full <- risk_estimator(fhat_ful, Y, type = type, na.rm = na.rm)
-    risk_reduced <- risk_estimator(fhat_red, Y, type = type, na.rm = na.rm)
-    risk_ci_full <- vimp_ci(risk_full, se = vimp_se(risk_update(fhat_ful, Y, weights = weights, type = type, na.rm = na.rm)), level = 1 - alpha)
-    risk_ci_reduced <- vimp_ci(risk_reduced, se = vimp_se(risk_update(fhat_red, Y, weights = weights, type = type, na.rm = na.rm)), level = 1 - alpha)
-  }
-  ## compute the update
-  update <- vimp_update(fhat_ful, fhat_red, Y, type = type, na.rm = na.rm)
+    ## perform a hypothesis test against the null of zero importance
+    if (!is.null(fhat_split_ful) & !is.null(fhat_split_red) & full_type != "anova") {
+        hyp_test <- vimp_hypothesis_test(fhat_split_ful, fhat_split_red, Y, folds, weights = weights, type = full_type, alpha = alpha, na.rm = na.rm)  
+    } else {
+        hyp_test <- list(test = NA, p_value = NA, risk_full = NA, risk_reduced = NA)
+    }
   
-  ## compute the standard error
-  se <- vimp_se(update, na.rm = na.rm)
+    ## get the call
+    cl <- match.call()
   
-  ## compute the confidence interval
-  ci <- vimp_ci(est, se, level = 1 - alpha)
-  
-  ## perform a hypothesis test against the null of zero importance
-  if (!is.null(fhat_split_ful) & !is.null(fhat_split_red) & type != "anova") {
-    hyp_test <- vimp_hypothesis_test(fhat_split_ful, fhat_split_red, Y, folds, weights = weights, type = type, alpha = alpha, na.rm = na.rm)  
-  } else {
-    hyp_test <- list(test = NA, p_value = NA, risk_full = NA, risk_reduced = NA)
-  }
-  
-  ## get the call
-  cl <- match.call()
-  
-  ## create the output and return it
-  output <- list(call = cl, s = indx,
+    ## create the output and return it
+    output <- list(call = cl, s = indx,
                  SL.library = SL.library,
                  full_fit = fhat_ful, red_fit = fhat_red, 
                  est = est,
@@ -223,8 +258,8 @@ vim <- function(Y, X, f1 = NULL, f2 = NULL, indx = 1, weights = rep(1, length(Y)
                  y = Y,
                  weights = weights)
   
-  ## make it also an vim and vim_type object
-  tmp.cls <- class(output)
-  class(output) <- c("vim", type, tmp.cls)
-  return(output)
+    ## make it also an vim and vim_type object
+    tmp.cls <- class(output)
+    class(output) <- c("vim", full_type, tmp.cls)
+    return(output)
 }
