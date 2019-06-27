85b53c3f702701bc29376c02aae15d1c4795826c
Brian Williamson <brianw26@uw.edu>
2019-06-27 14:32:25 -0700

    compute update for deviance, r_squared

--- R/cv_vimp_update.R
+++ R/cv_vimp_update.R
@@ -41,10 +41,40 @@ cv_vimp_update <- function(full, reduced, y, folds, weights = rep(1, length(y)),
     if (full_type != "anova" & full_type != "mse" & full_type != "cross_entropy") {
         ic <- ic_full - ic_redu
     } else if (full_type == "cross_entropy") {
-        ic <- 
+        ## denominator
+        if (is.null(dim(y))) { # assume that zero is in first column
+            y_mult <- cbind(1 - y, y)
+        } else {
+            y_mult <- y
+        }
+        p <- apply(y_mult, 2, mean, na.rm = na.rm)
+        denom_point_est <- (-1)*sum(log(p))
+        ic_denom <- rowSums(-1/p*((y_mult == 1) - p))
+
+        ## cross-entropies
+        cross_entropy_full <- cv_predictiveness_point_est(full, y, folds, full_type, na.rm)
+        cross_entropy_redu <- cv_predictiveness_point_est(reduced, y, folds, full_type, na.rm)
+        ic_ce_full <- cv_predictiveness_update(full, y, folds, full_type, na.rm)
+        ic_ce_redu <- cv_predictiveness_update(reduced, y, folds, full_type, na.rm)
+
+        ## influence curve
+        grad <- matrix(c(1/denom_point_est, -cross_entropy_full/denom_point_est^2,
+                         -1/denom_point_est, cross_entropy_redu/denom_point_est^2), nrow = 1)
+        ic <- as.vector(grad %*% t(cbind(ic_ce_full, ic_denom, ic_ce_redu, ic_denom)))
     } else if (full_type == "mse") {
+        ## variance
         var <- mean((y - mean(y, na.rm = na.rm))^2, na.rm = na.rm)
-        mse <- cv_predictiveness_point_est()
+        ic_var <- (y - mean(y, na.rm = na.rm))^2 - var
+
+        ## mses
+        mse_full <- cv_predictiveness_point_est(full, y, folds, full_type, na.rm)
+        ic_mse_full <- cv_predictiveness_update(full, y, folds, full_type, na.rm)
+        mse_redu <- cv_predictiveness_point_est(reduced, y, folds, full_type, na.rm)
+        ic_mse_redu <- cv_predictiveness_update(reduced, y, folds, full_type, na.rm)
+
+        ## influence curve
+        grad <- matrix(c(1/var, -mse_full/var^2, -1/var, mse_redu/var^2), nrow = 1)
+        ic <- as.vector((-1)*(grad %*% t(cbind(ic_mse_full, ic_var, ic_mse_redu, ic_var))))
     } else {
         ic <- 2*(y - full)*(full - reduced) + (full - reduced) ^ 2 - mean((full - reduced) ^ 2, na.rm = na.rm)
     }
