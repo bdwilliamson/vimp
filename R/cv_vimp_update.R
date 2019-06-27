b3e1fccdf8106b99a125ff1ee640181f046e31c4
Brian Williamson <brianw26@uw.edu>
2019-06-27 16:48:59 -0700

    include placeholder for all ics

--- R/cv_vimp_update.R
+++ R/cv_vimp_update.R
@@ -29,7 +29,9 @@ cv_vimp_update <- function(full, reduced, y, folds, weights = rep(1, length(y)),
     ic_full_lst <- cv_predictiveness_update(full, y, folds, weights, type, na.rm)
     ic_redu_lst <- cv_predictiveness_update(reduced, y, folds, weights, type, na.rm)
     ic_full <- ic_full_lst$ic
-    ic_redu <- ic_full_lst$ic
+    ic_redu <- ic_redu_lst$ic
+    ics_full <- ic_full_lst$all_ics
+    ics_redu <- ic_redu_lst$all_ics
     # V <- length(unique(folds))
     # max_nrow <- max(apply(matrix(1:V), 1, function(x) length(y[folds == x])))
     # ics_full <- matrix(NA, nrow = max_nrow, ncol = V)
@@ -47,6 +49,7 @@ cv_vimp_update <- function(full, reduced, y, folds, weights = rep(1, length(y)),
     ## if type isn't anova, return
     if (full_type != "anova" & full_type != "mse" & full_type != "cross_entropy") {
         ic <- ic_full - ic_redu
+        ics <- ics_full - ics_redu
     } else if (full_type == "cross_entropy") {
         ## denominator
         if (is.null(dim(y))) { # assume that zero is in first column
@@ -61,13 +64,12 @@ cv_vimp_update <- function(full, reduced, y, folds, weights = rep(1, length(y)),
         ## cross-entropies
         cross_entropy_full <- cv_predictiveness_point_est(full, y, folds, full_type, na.rm)$point_est
         cross_entropy_redu <- cv_predictiveness_point_est(reduced, y, folds, full_type, na.rm)$point_est
-        ic_ce_full <- cv_predictiveness_update(full, y, folds, full_type, na.rm)$ic
-        ic_ce_redu <- cv_predictiveness_update(reduced, y, folds, full_type, na.rm)$ic
 
         ## influence curve
         grad <- matrix(c(1/denom_point_est, -cross_entropy_full/denom_point_est^2,
                          -1/denom_point_est, cross_entropy_redu/denom_point_est^2), nrow = 1)
-        ic <- as.vector(grad %*% t(cbind(ic_ce_full, ic_denom, ic_ce_redu, ic_denom)))
+        ic <- as.vector(grad %*% t(cbind(ic_full, ic_denom, ic_redu, ic_denom)))
+        ics <- 
     } else if (full_type == "mse") {
         ## variance
         var <- mean((y - mean(y, na.rm = na.rm))^2, na.rm = na.rm)
@@ -75,15 +77,28 @@ cv_vimp_update <- function(full, reduced, y, folds, weights = rep(1, length(y)),
 
         ## mses
         mse_full <- cv_predictiveness_point_est(full, y, folds, full_type, na.rm)$point_est
-        ic_mse_full <- cv_predictiveness_update(full, y, folds, weights, full_type, na.rm)$ic
         mse_redu <- cv_predictiveness_point_est(reduced, y, folds, full_type, na.rm)$point_est
-        ic_mse_redu <- cv_predictiveness_update(reduced, y, folds, weights, full_type, na.rm)$ic
 
         ## influence curve
         grad <- matrix(c(1/var, -mse_full/var^2, -1/var, mse_redu/var^2), nrow = 1)
-        ic <- as.vector((-1)*(grad %*% t(cbind(ic_mse_full, ic_var, ic_mse_redu, ic_var))))
+        ic <- as.vector((-1)*(grad %*% t(cbind(ic_full, ic_var, ic_redu, ic_var))))
     } else {
-        ic <- 2*(y - full)*(full - reduced) + (full - reduced) ^ 2 - mean((full - reduced) ^ 2, na.rm = na.rm)
+        V <- length(unique(folds))
+        max_nrow <- max(apply(matrix(1:V), 1, function(x) length(y[folds == x])))
+        ics <- matrix(NA, nrow = max_nrow, ncol = V)
+        # ics_full <- matrix(NA, nrow = max_nrow, ncol = V)
+        # ics_redu <- matrix(NA, nrow = max_nrow, ncol = V)
+        # ## if r_squared or deviance, change to MSE or cross-entropy
+        # if (full_type == "r_squared") full_type <- "mse"
+        # if (full_type == "deviance") full_type <- "cross_entropy"
+        for (v in 1:V) {
+        #     ics_full[1:length(y[folds == v]), v] <- predictiveness_update(full[[v]], y[folds == v], weights[folds == v], full_type, na.rm)
+        #     ics_redu[1:length(y[folds == v]), v] <- predictiveness_update(reduced[[v]], y[folds == v], weights[folds == v], full_type, na.rm)    
+            ics[1:length(y[folds == v]), v] <- 2*(y[folds == v] - full[[v]])*(full[[v]] - reduced[[v]]) + (full[[v]] - reduced[[v]]) ^ 2 - mean((full[[v]] - reduced[[v]]) ^ 2, na.rm = na.rm)
+        }
+        # ic_full <- rowMeans(ics_full, na.rm = TRUE)
+        # ic_redu <- rowMeans(ics_redu, na.rm = TRUE)
+        ic <- rowMeans(ics, na.rm = TRUE)
     }
-    return(weights*ic)
+    return(list(ic = weights*ic, all_ics = ics))
 }
