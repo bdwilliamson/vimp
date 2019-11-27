ddf20af758b279707abcf12b4eac8ffcd27b55f2
Brian Williamson <brianw26@uw.edu>
2019-11-27 13:43:22 -0800

    play nicely with new cv

--- tests/testthat/test-cv_vim.R
+++ tests/testthat/test-cv_vim.R
@@ -20,7 +20,7 @@ learners <- c("SL.glm.interaction", "SL.xgboost1", "SL.mean")
 
 test_that("Cross-validated variable importance using internally-computed regressions works", {
   est <- cv_vim(Y = y, X = x, indx = 2, V = 3, type = "r_squared", run_regression = TRUE,
-                SL.library = learners, alpha = 0.05, delta = 1e-3, cvControl = list(V = 3),
+                SL.library = learners, alpha = 0.05, delta = 0, cvControl = list(V = 3),
                 env = environment(), na.rm = TRUE)
   ## check variable importance estimate
   expect_equal(est$est, (500/729)/(1 + 2497/7875 + 500/729), tolerance = 0.1)
@@ -42,31 +42,33 @@ test_that("Cross-validated variable importance using internally-computed regress
 ## set up the folds
 indx <- 2
 V <- 3
+Y <- matrix(y)
 set.seed(4747)
-folds <- rep(seq_len(V), length = n)
-folds <- sample(folds)
-## get the fitted values by fitting the super learner on each pair
+outer_folds <- rep_len(seq_len(2), dim(Y)[1])
+outer_folds <- sample(outer_folds)
+inner_folds_1 <- rep_len(seq_len(V), dim(Y[outer_folds == 1, , drop = FALSE])[1])
+inner_folds_1 <- sample(inner_folds_1)
+inner_folds_2 <- rep_len(seq_len(V), dim(Y[outer_folds == 2, , drop = FALSE])[1])
+inner_folds_2 <- sample(inner_folds_2)
+## fit the super learner on each full/reduced pair
 fhat_ful <- list()
 fhat_red <- list()
-Y <- matrix(y)
 for (v in 1:V) {
   ## fit super learner
-  fit <- SuperLearner::SuperLearner(Y = Y[folds != v, , drop = FALSE],
-                                    X = x[folds != v, , drop = FALSE], SL.library = learners, cvControl = list(V = 3))
+  fit <- SuperLearner::SuperLearner(Y = Y[outer_folds == 1, , drop = FALSE][inner_folds_1 != v, , drop = FALSE], X = x[outer_folds == 1, , drop = FALSE][inner_folds_1 != v, , drop = FALSE], SL.library = learners, cvControl = list(V = V))
   fitted_v <- SuperLearner::predict.SuperLearner(fit)$pred
   ## get predictions on the validation fold
-  fhat_ful[[v]] <- SuperLearner::predict.SuperLearner(fit,
-                                                      newdata = x[folds == v, , drop = FALSE])$pred
+  fhat_ful[[v]] <- SuperLearner::predict.SuperLearner(fit, newdata = x[outer_folds == 1, , drop = FALSE][inner_folds_1 == v, , drop = FALSE])$pred
   ## fit the super learner on the reduced covariates
-  red <- SuperLearner::SuperLearner(Y = fitted_v,
-                                    X = x[folds != v, -indx, drop = FALSE], SL.library = learners, cvControl = list(V = 3))
+  fit_2 <- SuperLearner::SuperLearner(Y = Y[outer_folds == 2, , drop = FALSE][inner_folds_2 != v, , drop = FALSE], X = x[outer_folds == 2, , drop = FALSE][inner_folds_2 != v, , drop = FALSE], SL.library = learners, cvControl = list(V = V))
+  fitted_v_2 <- SuperLearner::predict.SuperLearner(fit_2)$pred
+  red <- SuperLearner::SuperLearner(Y = fitted_v_2, X = x[outer_folds == 2, , drop = FALSE][inner_folds_2 != v, -indx, drop = FALSE], SL.library = learners, cvControl = list(V = V))
   ## get predictions on the validation fold
-  fhat_red[[v]] <- SuperLearner::predict.SuperLearner(red,
-                                                      newdata = x[folds == v, -indx, drop = FALSE])$pred
+  fhat_red[[v]] <- SuperLearner::predict.SuperLearner(red, newdata = x[outer_folds == 2, , drop = FALSE][inner_folds_2 == v, -indx, drop = FALSE])$pred
 }
+folds <- list(outer_folds = outer_folds, inner_folds = list(inner_folds_1, inner_folds_2))
 test_that("Cross-validated variable importance using externally-computed regressions works", {
-  est <- cv_vim(Y = y, f1 = fhat_ful, f2 = fhat_red, indx = 2, delta = 1e-3,
-  V = 3, folds = folds, type = "r_squared", run_regression = FALSE, alpha = 0.05, na.rm = TRUE)
+  est <- cv_vim(Y = y, f1 = fhat_ful, f2 = fhat_red, indx = 2, delta = 0, V = 3, folds = folds, type = "r_squared", run_regression = FALSE, alpha = 0.05, na.rm = TRUE)
   ## check variable importance estimate
   expect_equal(est$est, (500/729)/(1 + 2497/7875 + 500/729), tolerance = 0.1)
   ## check full predictiveness estimate
@@ -81,15 +83,15 @@ test_that("Cross-validated variable importance using externally-computed regress
   expect_silent(format(est)[1])
   expect_output(print(est), "Estimate", fixed = TRUE)
   ## check that influence curve worked
-  expect_length(est$update, length(y))
+  expect_length(est$update, length(y) / 2)
 })
 
 test_that("Measures of predictiveness work", {
-  full_rsquared <- cv_predictiveness_point_est(fhat_ful, y, folds = folds, type = "r_squared", na.rm = TRUE)
+  full_rsquared <- cv_predictiveness_point_est(fhat_ful, y[outer_folds == 1], folds = inner_folds_1, type = "r_squared", na.rm = TRUE)
   expect_equal(full_rsquared$point_est, 0.44, tolerance = 0.1)
   expect_length(full_rsquared$all_ests, V)
-  full_update <- cv_predictiveness_update(fhat_red, y, folds = folds, type = "r_squared", na.rm = TRUE)
-  expect_length(full_update$ic, length(y))
+  full_update <- cv_predictiveness_update(fhat_red, y[outer_folds == 1], folds = inner_folds_1, type = "r_squared", na.rm = TRUE)
+  expect_length(full_update$ic, length(y) / 2)
   expect_equal(dim(full_update$all_ics)[2], V)
 })
 
