f720b0df8c012100d4cfb3d3d4d82c1dd0349e4d
Brian Williamson <brianw26@uw.edu>
2020-03-06 12:23:04 -0800

    update documentation, add spvim tests

--- R/sp_vim.R
+++ R/sp_vim.R
@@ -13,7 +13,6 @@
 #' @param gamma the fraction of the sample size to use when sampling subsets (e.g., \code{gamma = 1} samples the same number of subsets as the sample size)
 #' @param alpha the level to compute the confidence interval at. Defaults to 0.05, corresponding to a 95\% confidence interval.
 #' @param delta the value of the \eqn{\delta}-null (i.e., testing if importance < \eqn{\delta}); defaults to 0.
-#' @param scale should CIs be computed on original ("identity") or logit ("logit") scale?
 #' @param na.rm should we remove NA's in the outcome and fitted values in computation? (defaults to \code{FALSE})
 #' @param ... other arguments to the estimation tool, see "See also".
 #'
@@ -28,12 +27,13 @@
 #' \itemize{
 #'  \item{call}{ - the call to \code{cv_vim}}
 #'  \item{SL.library}{ - the library of learners passed to \code{SuperLearner}}
-#' \item{v_lst}{- the estimated predictiveness measure for each sampled subset}
+#' \item{v}{- the estimated predictiveness measure for each sampled subset}
 #'  \item{preds_lst}{ - the predicted values from the chosen method for each sampled subset}
-#'  \item{est}{ - the estimated variable importance}
+#'  \item{est}{ - the estimated SPVIM value for each feature}
 #'  \item{ic_lst}{ - the influence functions for each sampled subset}
-#'  \item{ses}{ - the standard error for the estimated variable importance}
-#'  \item{cis}{ - the \eqn{(1-\alpha) \times 100}\% confidence intervals based on the variable importance estimates}
+#'  \item{ic}{- a list of the SPVIM influence function contributions}
+#'  \item{se}{ - the standard errors for the estimated variable importance}
+#'  \item{ci}{ - the \eqn{(1-\alpha) \times 100}\% confidence intervals based on the variable importance estimates}
 #'  \item{gamma}{- the fraction of the sample size used when sampling subsets}
 #'  \item{alpha}{ - the level, for confidence interval calculation}
 #'  \item{delta}{- the \code{delta} value used for hypothesis testing}
@@ -70,7 +70,7 @@
 #' }
 #' @seealso \code{\link[SuperLearner]{SuperLearner}} for specific usage of the \code{SuperLearner} function and package.
 #' @export
-sp_vim <- function(Y, X, V = 5, weights = rep(1, length(Y)), type = "r_squared", SL.library = c("SL.glmnet", "SL.xgboost", "SL.mean"), gamma = 1, alpha = 0.05, delta = 0, scale = "identity", na.rm = FALSE, ...) {
+sp_vim <- function(Y, X, V = 5, weights = rep(1, length(Y)), type = "r_squared", SL.library = c("SL.glmnet", "SL.xgboost", "SL.mean"), gamma = 1, alpha = 0.05, delta = 0, na.rm = FALSE, ...) {
     ## check to see if f1 and f2 are missing
     ## if the data is missing, stop and throw an error
     if (missing(Y)) stop("You must enter an outcome, Y.")
@@ -101,10 +101,10 @@ sp_vim <- function(Y, X, V = 5, weights = rep(1, length(Y)), type = "r_squared",
     ## get v, preds, ic for null set
     preds_none <- list()
     for (v in 1:V) {
-        preds_none[[v]] <- rep(mean(Y[outer_folds == 1, ][inner_folds_1 == v]), sum(inner_folds_1 == v))
+        preds_none[[v]] <- rep(mean(Y[outer_folds == 2, ][inner_folds_2 == v]), sum(inner_folds_2 == v))
     }
-    v_none <- cv_predictiveness_point_est(fitted_values = preds_none, y = Y[outer_folds == 1, , drop = FALSE], folds = inner_folds_1, weights = weights[outer_folds == 1], type = full_type, na.rm = na.rm)$point_est
-    ic_none <- cv_predictiveness_update(preds_none, Y[outer_folds == 1, , drop = FALSE], inner_folds_1, weights[outer_folds == 1], type = full_type, na.rm = na.rm)$ic
+    v_none <- cv_predictiveness_point_est(fitted_values = preds_none, y = Y[outer_folds == 2, , drop = FALSE], folds = inner_folds_2, weights = weights[outer_folds == 2], type = full_type, na.rm = na.rm)$point_est
+    ic_none <- cv_predictiveness_update(preds_none, Y[outer_folds == 2, , drop = FALSE], inner_folds_2, weights[outer_folds == 2], type = full_type, na.rm = na.rm)$ic
 
     ## get v, preds, ic for remaining non-null groups in S
     preds_lst <- lapply(S[-1], function(s) run_sl(Y[outer_folds == 2, , drop = FALSE], X[outer_folds == 2, ], V = V, SL.library = SL.library, s = s, folds = inner_folds_2, ...))
@@ -123,70 +123,66 @@ sp_vim <- function(Y, X, V = 5, weights = rep(1, length(Y)), type = "r_squared",
     kkt_matrix <- rbind(cbind(kkt_matrix_11, kkt_matrix_12), cbind(kkt_matrix_21, kkt_matrix_22))
     ls_matrix <- rbind(2 * t(A_W) %*% v_W, c_n)
     ls_solution <- solve(kkt_matrix) %*% ls_matrix
-    est <- ls_solution[2:dim(X)[2], ]
-    lambdas <- ls_solution[(dim(X)[2] + 1):dim(ls_solution)[1], ]
-    
+    est <- ls_solution[1:(ncol(X) + 1), ]
+    lambdas <- ls_solution[(ncol(X) + 2):dim(ls_solution)[1], ]
+
+    ## compute the SPVIM ICs
+    ic_mat <- do.call(rbind, c(list(ic_none), ic_lst))
+    ics <- spvim_ics(Z, z_counts, W, v, est, G, c_n, ic_mat, full_type)
+
     ## calculate the standard error
-    se <- vimp_se(est, update, scale = scale, na.rm = na.rm)
-    ses <- vector("numeric", length = V)
-    for (v in 1:V) {
-        ses[v] <- vimp_se(all_ests[v], updates[, v], scale = scale, na.rm = na.rm)
+    ses <- vector("numeric", ncol(X) + 1)
+    for (j in 1:(ncol(X) + 1)) {
+        ses[j] <- spvim_se(ics, j, gamma = gamma, na_rm = na.rm)
     }
     ## if est < 0, set to zero and print warning
-    if (est < 0) {
-        est <- 0
-        warning("Original estimate < 0; returning zero.")
+    if (any(est < 0)) {
+        est[est < 0] <- 0
+        warning("One or more original estimates < 0; returning zero for these indices.")
     }
 
-    ## calculate the confidence interval
-    ci <- vimp_ci(est, se, scale = scale, 1 - alpha)
-    predictiveness_ci_full <- vimp_ci(predictiveness_full, se = vimp_se(predictiveness_full, cv_predictiveness_update(fhat_ful, Y[outer_folds == 1, , drop = FALSE], inner_folds_1, weights[outer_folds == 1], type = full_type, na.rm = na.rm)$ic, scale = scale), scale = scale, level = 1 - alpha)
-    predictiveness_ci_redu <- vimp_ci(predictiveness_redu, se = vimp_se(predictiveness_redu, cv_predictiveness_update(fhat_red, Y[outer_folds == 2, , drop = FALSE], inner_folds_2, weights[outer_folds == 2], type = full_type, na.rm = na.rm)$ic, scale = scale), scale = scale, level = 1 - alpha)
+    ## calculate the confidence intervals
+    cis <- vimp_ci(est[-1], ses[-1], scale = "identity", level = 1 - alpha)
 
     ## compute a hypothesis test against the null of zero importance
-    ## note that for full risk for fold 1 is first-order independent of the V-1 other reduced-fold risks
-    if (type == "regression" | type == "anova") {
-        hyp_test <- list(test = NA, p_value = NA, predictiveness_full = NA, predictiveness_reduced = NA, predictiveness_ci_full = rep(NA, 2), predictiveness_ci_reduced = rep(NA, 2), se_full = NA, se_redu = NA, test_statistic = NA)
-    } else {
-        ## reject iff ALL pairwise comparisons with the V-1 other risk CIs don't overlap
-        hyp_test <- vimp_hypothesis_test(fhat_ful, fhat_red, Y, folds, delta = delta, weights = weights, type = type, alpha = alpha, cv = TRUE, scale = scale, na.rm = na.rm)
+    preds_none_0 <- list()
+    for (v in 1:V) {
+        preds_none_0[[v]] <- rep(mean(Y[outer_folds == 1, ][inner_folds_1 == v]), sum(inner_folds_1 == v))
     }
-
+    v_none_0 <- cv_predictiveness_point_est(fitted_values = preds_none_0, y = Y[outer_folds == 1, , drop = FALSE], folds = inner_folds_1, weights = weights[outer_folds == 1], type = full_type, na.rm = na.rm)$point_est
+    ic_none_0 <- cv_predictiveness_update(preds_none_0, Y[outer_folds == 1, , drop = FALSE], inner_folds_1, weights[outer_folds == 1], type = full_type, na.rm = na.rm)$ic
+    se_none_0 <- sqrt(mean(ic_none_0 ^ 2, na.rm = na.rm)) / sqrt(sum(outer_folds == 1))
+    ## get shapley vals + null predictiveness
+    shapley_vals_plus <- est + est[1]
+    ses_one <- sqrt(ses ^ 2 + se_none_0 ^ 2)
+    test_statistics <- sapply(2:length(est), function(j, ests, ses, est_0, se_0, delta) {
+        (ests[j] - est_0 - delta) / sqrt(ses[j] ^ 2 + se_0 ^ 2)
+    }, ests = shapley_vals_plus, ses = ses_one, est_0 = v_none_0, se_0 = se_none_0, delta = delta)
+    p_values <- 1 - pnorm(test_statistics)
+    hyp_tests <- p_values < alpha
     ## get the call
     cl <- match.call()
 
     ## create the output and return it
     ## create output tibble
-    chr_indx <- paste(as.character(indx), collapse = ",")
-    mat <- tibble::tibble(s = chr_indx, est = est, se = se[1], cil = ci[1], ciu = ci[2], test = hyp_test$test, p_value = hyp_test$p_value)
-    output <- list(call = cl, s = chr_indx,
+    mat <- tibble::tibble(s = as.character(1:ncol(X)), est = est[-1], se = ses[-1], cil = cis[, 1],
+                          ciu = cis[, 2], test = hyp_tests, p_value = p_values)
+    output <- list(call = cl, s = as.character(1:ncol(X)),
                  SL.library = SL.library,
-                 full_fit = fhat_ful, red_fit = fhat_red,
+                 v = v,
+                 preds_lst = c(list(preds_none), preds_lst),
                  est = est,
-                 naive = naive,
-                 update = update,
-                 se = se, ci = ci,
-                 predictiveness_full = predictiveness_full,
-                 predictiveness_reduced = predictiveness_redu,
-                 predictiveness_ci_full = predictiveness_ci_full,
-                 predictiveness_ci_reduced = predictiveness_ci_redu,
-                 test = hyp_test$test,
-                 p_value = hyp_test$p_value,
-                 hyp_test_predictiveness_full = hyp_test$predictiveness_full,
-                 hyp_test_predictiveness_reduced = hyp_test$predictiveness_reduced,
-                 hyp_test_predictiveness_ci_full = hyp_test$predictiveness_ci_full,
-                 hyp_test_predictiveness_ci_reduced = hyp_test$predictiveness_ci_reduced,
-                 hyp_test_se_full = hyp_test$se_full,
-                 hyp_test_se_reduced = hyp_test$se_redu,
-                 test_statistic = hyp_test$test_statistic,
-                 full_mod = full,
-                 red_mod = reduced,
+                 ic_lst = c(list(ic_none), ic_lst),
+                 ic = ics,
+                 se = ses, ci = cis,
+                 test = hyp_tests,
+                 p_value = p_values,
+                 test_statistic = test_statistics,
+                 gamma = gamma,
                  alpha = alpha,
                  delta = delta,
-                 folds = folds,
                  y = Y,
                  weights = weights,
-                 scale = scale,
                  mat = mat)
 
     ## make it also an vim object
