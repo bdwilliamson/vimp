dac59b5968942f588ab6de52c4b7574f32fe8c5d
Brian Williamson <brianw26@uw.edu>
2020-04-24 09:26:41 -0700

    rename package description file, update package description file (not DESCRIPTION)

---
+++ R/vimp-package.R
@@ -0,0 +1,47 @@
+#' vimp: Perform Inference on Algorithm-Agnostic Variable Importance
+#'
+#' A unified framework for valid statistical inference on algorithm-agnostic
+#' measures of variable importance. You provide the data, a method for 
+#' estimating the conditional mean of the outcome given the covariates,
+#' choose a variable importance measure, and specify variable(s) of interest;
+#' 'vimp' takes care of the rest.
+#' 
+#' @section Author(s):
+#' \bold{Maintainer}: Brian Williamson \url{http://bdwilliamson.github.io}
+#' 
+#' Methodology authors:
+#' \itemize{
+#'   \item{Brian D. Williamson}
+#'   \item{Peter B. Gilbert}
+#'   \item{Noah R. Simon}
+#'   \item{Marco Carone}
+#' }
+#' 
+#' @section See Also:
+#' Preprints:
+#' \itemize{
+#'   \item{\url{http://biostats.bepress.com/uwbiostat/paper422/} (R-squared-based variable importance)}
+#'   \item{\url{http://arxiv.org/abs/2004.03683} (general variable importance)}
+#' }
+#' 
+#' Other useful links:
+#' \itemize{
+#'   \item{\url{http://bdwilliamson.github.io/vimp}}
+#'   \item{\url{http://github.com/bdwilliamson/vimp}}
+#'   \item{Report bugs at \url{http://github.com/bdwilliamson/vimp/issues}}
+#' }
+#' 
+#' @section Imports:
+#' The packages that we import either make the internal code nice (dplyr, magrittr, tibble, rlang), 
+#' are directly relevant to estimating the conditional mean (SuperLearner) or predictiveness measures (ROCR), 
+#' or are necessary for hypothesis testing (stats).
+#' 
+#' We suggest several other packages: xgboost, ranger, gam, glmnet, and quadprog allow 
+#' a flexible library of candidate learners in the Super Learner; ggplot2, cowplot, 
+#' and forcats help with plotting variable importance estimates; testthat and covr 
+#' help with unit tests; and knitr, rmarkdown,
+#' RCurl, and MASS help with the vignettes and examples.
+#'
+#' @docType package
+#' @name vimp
+NULL
