1076f6cdadd6a4af0b18b8904e830fa9b0fb1f30
Brian Williamson <brianw26@uw.edu>
2020-10-09 08:33:25 -0700

    add data.table to imports, add test?

--- tests/testthat/test-ipcw.R
+++ tests/testthat/test-ipcw.R
@@ -2,6 +2,7 @@
 library("testthat")
 library("SuperLearner")
 library("xgboost")
+library("data.table")
 library("vimp")
 
 # generate the data
@@ -15,11 +16,11 @@ y <- (x[,1]/5)^2*(x[,1]+7)/5 + (x[,2]/3)^2 + rnorm(n, 0, 1)
 
 # make this a two-phase study, assume that X is only measured on 
 # subjects in the second phase; note C = 1 is inclusion
-C <- sample(0:1, size = n, replace = TRUE)
+C <- rbinom(n, size = 1, prob = exp(y) / (1 + exp(y)))
+tmp_x <- x
+tmp_x[C == 0, ] <- NA
+x <- tmp_x
 ipc_weights <- 1 / predict(glm(C ~ y, family = "binomial"), type = "response")
-
-# sample-splitting folds, for hypothesis testing
-folds <- sample(rep(seq_len(2), length = length(y)))
 
 # set up a library for SuperLearner
 SL.xgboost1 <- function(..., max_depth = 1, ntree = 500, shrinkage = 0.1){
@@ -27,9 +28,21 @@ SL.xgboost1 <- function(..., max_depth = 1, ntree = 500, shrinkage = 0.1){
 }
 learners <- c("SL.glm.interaction", "SL.xgboost1", "SL.mean")
 
+true_vim <- (500/729)/(1 + 2497/7875 + 500/729)
+
+set.seed(1234)
 # test that VIM with inverse probability of coarsening weights works
 test_that("VIM with inverse probability of coarsening weights works", {
   est <- vim(Y = y, X = x, indx = 1, type = "r_squared", run_regression = TRUE, SL.library = learners,
-             alpha = 0.05, delta = 0, folds = folds, C = C, ipc_weights = ipc_weights,
-             ipc_fit_type = "SL", cvControl = list(V = 3))
+             alpha = 0.05, delta = 0, C = C, Z = "y", ipc_weights = ipc_weights,
+             cvControl = list(V = 3))
+  expect_equal(est$est, true_vim, tolerance = 0.2, scale = 1)
+})
+
+# test that VIM with inverse probability of coarsening weights works
+test_that("VIM with inverse probability of coarsening weights works", {
+  est <- vim(Y = y, X = x, indx = 1, type = "r_squared", run_regression = TRUE, SL.library = learners,
+             alpha = 0.05, delta = 0, C = C, Z = "y", ipc_weights = ipc_weights,
+             cvControl = list(V = 3))
+  expect_equal(est$est, true_vim, tolerance = 0.2, scale = 1)
 })
