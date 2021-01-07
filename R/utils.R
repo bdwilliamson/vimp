f27eeb4c8d15b6de2a36a90db667b8b73686f804
Brian Williamson <brianw26@uw.edu>
2021-01-07 13:57:17 -0800

    remove extra stuff from pre-release check note

--- R/utils.R
+++ R/utils.R
@@ -176,6 +176,6 @@ run_sl <- function(Y, X, V, SL.library, univariate_SL.library, s, folds,
 # @keywords internal
 release_questions <- function() {
   c(
-    "Have you run cran_prep <- rhub::check_for_cran(env_vars = c(R_COMPILE_AND_INSTALL_PACKAGES = 'always'))?"
+    "Have you run cran_prep <- rhub::check_for_cran(env_vars = c(R_COMPILE_AND_INSTALL_PACKAGES = 'always'), show_status = FALSE)?"
   )
 }
