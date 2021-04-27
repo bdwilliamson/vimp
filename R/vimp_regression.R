817d8e51a069ec574e877d56cb5326027a9a8df7
bdwilliamson <brianw26@uw.edu>
2021-04-27 11:20:52 -0700

    Update vimp_regression

--- R/vimp_regression.R
+++ R/vimp_regression.R
@@ -1,16 +1,16 @@
 #' Nonparametric Intrinsic Variable Importance Estimates: ANOVA
 #'
-#' Compute estimates of and confidence intervals for nonparametric 
-#' ANOVA-based intrinsic variable importance. This is a wrapper function for 
-#' \code{cv_vim}, with \code{type = "anova"}. 
+#' Compute estimates of and confidence intervals for nonparametric
+#' ANOVA-based intrinsic variable importance. This is a wrapper function for
+#' \code{cv_vim}, with \code{type = "anova"}.
 #' This function is deprecated in \code{vimp} version 2.0.0.
 #'
-#' @inheritParams cv_vim
+#' @inheritParams vimp_anova
 #'
-#' @return An object of classes \code{vim} and \code{vim_regression}. 
+#' @return An object of classes \code{vim} and \code{vim_regression}.
 #'   See Details for more information.
 #'
-#' @inherit cv_vim details
+#' @inherit vimp_anova details
 #'
 #' @examples
 #' # generate the data
@@ -28,7 +28,7 @@
 #' # set up a library for SuperLearner; note simple library for speed
 #' library("SuperLearner")
 #' learners <- c("SL.glm", "SL.mean")
-#' 
+#'
 #' # estimate (with a small number of folds, for illustration only)
 #' est <- vimp_regression(y, x, indx = 2,
 #'            alpha = 0.05, run_regression = TRUE,
@@ -37,25 +37,24 @@
 #' @seealso \code{\link[SuperLearner]{SuperLearner}} for specific usage of the \code{SuperLearner} function and package.
 #' @export
 vimp_regression <- function(Y = NULL, X = NULL, cross_fitted_f1 = NULL,
-                            cross_fitted_f2 = NULL, f1 = NULL, f2 = NULL, indx = 1, 
-                            V = 10, run_regression = TRUE, 
-                            SL.library = c("SL.glmnet", "SL.xgboost", "SL.mean"), 
-                            alpha = 0.05, delta = 0, na.rm = FALSE, 
-                            cross_fitting_folds = NULL, sample_splitting_folds = NULL,
-                            stratified = FALSE, C = rep(1, length(Y)), Z = NULL, 
-                            ipc_weights = rep(1, length(Y)), scale = "identity", 
-                            ipc_est_type = "aipw", ...) {
-  .Deprecated("vimp_anova", package = "vimp", 
+                       cross_fitted_f2 = NULL,  indx = 1,
+                       V = 10, run_regression = TRUE,
+                       SL.library = c("SL.glmnet", "SL.xgboost", "SL.mean"),
+                       alpha = 0.05, delta = 0, na.rm = FALSE,
+                       cross_fitting_folds = NULL,
+                       stratified = FALSE, C = rep(1, length(Y)), Z = NULL,
+                       ipc_weights = rep(1, length(Y)), scale = "identity",
+                       ipc_est_type = "aipw", ...) {
+  .Deprecated("vimp_anova", package = "vimp",
               msg = paste0(
                 "vimp_anova now performs all functionality of vimp_regression; "
                 , "please update any code to reflect this change!"
               ))
-  vimp_anova(Y = Y, X = X, cross_fitted_f1 = cross_fitted_f1, f1 = f1, f2 = f2, 
-             indx = indx, V = V, 
-             run_regression = run_regression, SL.library = SL.library, 
-             alpha = alpha, delta = delta, na.rm = na.rm, 
-             cross_fitting_folds = cross_fitting_folds, 
-             sample_splitting_folds = sample_splitting_folds,
-             stratified = stratified, C = C, Z = Z, ipc_weights = ipc_weights, 
+  vimp_anova(Y = Y, X = X, cross_fitted_f1 = cross_fitted_f1,
+             indx = indx, V = V,
+             run_regression = run_regression, SL.library = SL.library,
+             alpha = alpha, delta = delta, na.rm = na.rm,
+             cross_fitting_folds = cross_fitting_folds,
+             stratified = stratified, C = C, Z = Z, ipc_weights = ipc_weights,
              ipc_est_type = ipc_est_type, scale = scale, ...)
 }
