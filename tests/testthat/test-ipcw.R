544d2d320f28be664daaee5036cb9555d0fec932
bdwilliamson <brianw26@uw.edu>
2021-04-27 10:22:07 -0700

    Update the index for importance

--- tests/testthat/test-ipcw.R
+++ tests/testthat/test-ipcw.R
@@ -21,15 +21,13 @@ r2_full <- 1 - mse_full / true_var
 shapley_val_1 <- (1/2) * (r2_one - 0) + (1/2) * (r2_full - r2_two)
 shapley_val_2 <- (1/2) * (r2_two - 0) + (1/2) * (r2_full - r2_one)
 
-# get the true conditional VIM
-true_vim <- 0.75 ^ 2 * 1 / true_var
-
 # make this a two-phase study, assume that X is only measured on
 # subjects in the second phase; note C = 1 is inclusion
 C <- rbinom(n, size = 1, prob = exp(y) / (1 + exp(y)))
 tmp_x <- x
 tmp_x[C == 0, ] <- NA
 x <- tmp_x
+x_df <- as.data.frame(x)
 ipc_weights <- 1 / predict(glm(C ~ y, family = "binomial"), type = "response")
 
 learners <- c("SL.glm", "SL.mean")
@@ -38,27 +36,27 @@ V <- 2
 set.seed(1234)
 # test that VIM with inverse probability of coarsening weights works
 test_that("VIM with inverse probability of coarsening weights works", {
-  est <- vim(Y = y, X = x, indx = 1, type = "r_squared", run_regression = TRUE, 
+  est <- vim(Y = y, X = x_df, indx = 1, type = "r_squared", run_regression = TRUE, 
              SL.library = learners,
              alpha = 0.05, delta = 0, C = C, Z = "Y", ipc_weights = ipc_weights,
              cvControl = list(V = V), env = environment())
-  expect_equal(est$est, true_vim, tolerance = 0.2, scale = 1)
+  expect_equal(est$est, r2_one, tolerance = 0.2, scale = 1)
 })
 set.seed(5678)
 # test that VIM with inverse probability of coarsening weights and cross-fitting works
 test_that("VIM with inverse probability of coarsening weights and cross-fitting works", {
-  est_cv <- cv_vim(Y = y, X = x, indx = 1, type = "r_squared", V = 2, 
+  est_cv <- cv_vim(Y = y, X = x_df, indx = 1, type = "r_squared", V = 2, 
                    run_regression = TRUE, SL.library = learners,
              alpha = 0.05, delta = 0, C = C, Z = "Y", ipc_weights = ipc_weights,
              cvControl = list(V = V), env = environment())
-  expect_equal(est_cv$est, true_vim, tolerance = 0.3, scale = 1)
+  expect_equal(est_cv$est, r2_one, tolerance = 0.3, scale = 1)
 })
 
 univariate_learners <- "SL.glm"
 set.seed(91011)
 # test that SPVIM with inverse probability of coarsening weights works
 test_that("SPVIM with inverse probability of coarsening weights works", {
-    est_spvim <- sp_vim(Y = y, X = x, type = "r_squared", V = 2, 
+    est_spvim <- sp_vim(Y = y, X = x_df, type = "r_squared", V = 2, 
                       SL.library = learners,
                       univariate_SL.library = univariate_learners, gamma = 0.1,
                       alpha = 0.05, delta = 0, C = C, Z = "Y", 
