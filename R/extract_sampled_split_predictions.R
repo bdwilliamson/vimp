bf85fa5c34722590a0e9fb9582964f2650eaaf6b
Brian Williamson <bwillia2@fredhutch.org>
2022-07-26 17:01:20 -0700

    Add option to return vector rather than list

--- R/extract_sampled_split_predictions.R
+++ R/extract_sampled_split_predictions.R
@@ -12,19 +12,21 @@
 #' @param sample_splitting_folds A vector of folds to use for sample splitting
 #' @param full logical; is this the fit to all covariates (\code{TRUE}) or not
 #'   (\code{FALSE})?
+#' @param vector logical; should we return a vector (where each element is 
+#'   the prediction when the corresponding row is in the validation fold) or a 
+#'   list?
 #'
 #' @seealso \code{\link[SuperLearner]{CV.SuperLearner}} for usage of the
 #'   \code{CV.SuperLearner} function.
-#' @return The predictions on validation data in each split-sample fold; a
-#'   list of length two, each element of which is a list with the predictions
-#'   on the split-sample cross-validation data.
+#' @return The predictions on validation data in each split-sample fold.
 #' @export
 extract_sampled_split_predictions <- function(cvsl_obj = NULL,
                                               sample_splitting = TRUE,
                                               sample_splitting_folds = NULL,
                                               full = TRUE,
                                               preds = NULL,
-                                              cross_fitting_folds = NULL) {
+                                              cross_fitting_folds = NULL,
+                                              vector = FALSE) {
   if ((is.null(cvsl_obj) | !(class(cvsl_obj) == "CV.SuperLearner")) & is.null(preds)) {
     stop("Please enter a CV.SuperLearner object or the predictions and folds from such an object.")
   } else if (!is.null(preds) & is.null(cross_fitting_folds)) {
