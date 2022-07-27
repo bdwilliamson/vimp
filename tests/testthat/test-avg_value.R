7ca0105cdbcc297b3144df79371ad81d4b0beeeb
Brian Williamson <bwillia2@fredhutch.org>
2022-07-26 17:01:34 -0700

    Towards a test of cv avg value

--- tests/testthat/test-avg_value.R
+++ tests/testthat/test-avg_value.R
@@ -24,6 +24,9 @@ avg_value_2 <- true_avg_value - mean(y_f2)
 # set up a library for SuperLearner
 learners <- c("SL.glm.interaction")
 V <- 2
+# cv folds
+folds <- make_folds(y = y, V = 2)
+sl_folds <- lapply(as.list(1:2), function(i) which(folds == i))
 
 set.seed(1234)
 # estimate the outcome regression
@@ -44,6 +47,49 @@ f_n2 <- as.numeric(predict(q_n2, newdata = cbind.data.frame(a = 1, x[, -2, drop 
 g_n <- SuperLearner::SuperLearner(Y = f_n, X = x, SL.library = learners, cvControl = list(V = V))
 g_n1 <- SuperLearner::SuperLearner(Y = f_n1, X = x[, -1, drop = FALSE], SL.library = learners, cvControl = list(V = V))
 g_n2 <- SuperLearner::SuperLearner(Y = f_n2, X = x[, -2, drop = FALSE], SL.library = learners, cvControl = list(V = V))
+
+# also get cross-fitted versions
+q_n_cv <- SuperLearner::CV.SuperLearner(Y = y, X = cbind.data.frame(a = a, x),
+                                        SL.library = learners, cvControl = list(V = 2 * V),
+                                        innerCvControl = list(list(V = V)),
+                                        saveAll = TRUE, control = list(saveFitLibrary = TRUE))
+cross_fitting_folds <- get_cv_sl_folds(q_n_cv$folds)
+q_n_cv_preds_lst <- lapply(as.list(seq_len(length(q_n_cv$AllSL))), function(l) {
+  as.numeric(predict(q_n_cv$AllSL[[l]], newdata = cbind.data.frame(a = 1, x[cross_fitting_folds == l, ]))$pred > 
+               predict(q_n_cv$AllSL[[l]], newdata = cbind.data.frame(a = 0, x[cross_fitting_folds == l, ]))$pred)
+})
+f_n_cv <- unlist(q_n_cv_preds_lst)[unlist(q_n_cv$folds)]
+g_n_cv <- SuperLearner::CV.SuperLearner(Y = f_n_cv, X = x, SL.library = learners,
+                                        cvControl = list(V = 2 * V, validRows = q_n_cv$folds), innerCvControl = list(list(V = V)),
+                                        saveAll = TRUE, control = list(saveFitLibrary = TRUE))
+opt_q_n_cv <- unlist(lapply(as.list(seq_len(length(q_n_cv$AllSL))), function(l) {
+  predict(q_n_cv$AllSL[[l]], newdata = cbind.data.frame(a = f_n_cv, x))$pred
+}))[unlist(q_n_cv$folds)]
+opt_g_n_cv <- unlist(lapply(as.list(seq_len(length(g_n_cv$AllSL))), function(l) {
+  predict(g_n_cv$AllSL[[l]], newdata = cbind.data.frame(x))$pred
+}))[unlist(q_n_cv$folds)]
+nuisances_cv <- list(f_n = f_n_cv, q_n = opt_q_n_cv, g_n = opt_g_n_cv)
+
+q_n_cv1 <- SuperLearner::CV.SuperLearner(Y = y, X = cbind.data.frame(a = a, x[, -1, drop = FALSE]),
+                                        SL.library = learners, cvControl = list(V = 2 * V, validRows = q_n_cv$folds),
+                                        innerCvControl = list(list(V = V)),
+                                        saveAll = TRUE, control = list(saveFitLibrary = TRUE))
+q_n_cv_preds_lst1 <- lapply(as.list(seq_len(length(q_n_cv1$AllSL))), function(l) {
+  as.numeric(predict(q_n_cv1$AllSL[[l]], newdata = cbind.data.frame(a = 1, x[cross_fitting_folds == l, -1, drop = FALSE]))$pred > 
+               predict(q_n_cv1$AllSL[[l]], newdata = cbind.data.frame(a = 0, x[cross_fitting_folds == l, -1, drop = FALSE]))$pred)
+})
+f_n_cv1 <- unlist(q_n_cv_preds_lst1)[unlist(q_n_cv1$folds)]
+g_n_cv1 <- SuperLearner::CV.SuperLearner(Y = f_n_cv, X = x[, -1, drop = FALSE], SL.library = learners,
+                                        cvControl = list(V = 2 * V, validRows = q_n_cv$folds), innerCvControl = list(list(V = V)),
+                                        saveAll = TRUE, control = list(saveFitLibrary = TRUE))
+opt_q_n_cv1 <- unlist(lapply(as.list(seq_len(length(q_n_cv1$AllSL))), function(l) {
+  predict(q_n_cv1$AllSL[[l]], newdata = cbind.data.frame(a = f_n_cv1, x[, -1, drop = FALSE]))$pred
+}))[unlist(q_n_cv1$folds)]
+opt_g_n_cv1 <- unlist(lapply(as.list(seq_len(length(g_n_cv1$AllSL))), function(l) {
+  predict(g_n_cv1$AllSL[[l]], newdata = cbind.data.frame(x[, -1, drop = FALSE]))$pred
+}))[unlist(q_n_cv1$folds)]
+nuisances_cv1 <- list(f_n = f_n_cv1, q_n = opt_q_n_cv1, g_n = opt_g_n_cv1)
+
 # test average value -----------------------------------------------------------
 # estimate the average value
 nuisances <- list(f_n = f_n, q_n = predict(q_n, newdata = cbind.data.frame(a = f_n, x))$pred,
@@ -74,6 +120,12 @@ test_that("Average-value-based VIM works", {
                        nuisance_estimators_reduced = nuisances1)
   expect_equal(vim_avg_value$est, avg_value_1, tol = 0.1, scale = 1)
 })
+
 test_that("Average-value-based VIM with cross-fitting works", {
-  
+  vim_avg_value_cv <- cv_vim(Y = y, X = cbind.data.frame(a = a, x), cross_fitted_f1 = f_n_cv,
+                             cross_fitted_f2 = f_n_cv1, indx = 1, type = "average_value",
+                             run_regression = FALSE, exposure_name = "a",
+                             nuisance_estimators_full = nuisances_cv,
+                             nuisance_estimators_reduced = nuisances_cv1,
+                             cross_fitting_folds = cross_fitting_folds)
 })
